593ed1b9f7d5378bc30d7b39e995c8dc493a9a28
Gspr-bit <gasparhs400@gmail.com>
2023-11-28 03:58:17 -0600

    Agrgue un 4 numero

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 name = input("Ingresa tu nombre")
 print(f"Hola {name}")
 
-print("Ingresa dos numeros")
+print("Ingresa cuatro numeros")
 a = int(input())
 b = int(input())
+c = int(input())
+d = int(input())
+
+print(f"El resultado de {a}+{b}+{c}+{d} = {a+b+c+d}")
 
